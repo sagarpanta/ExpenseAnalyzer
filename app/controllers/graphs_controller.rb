62083da238cd2346db1910d4dea8e2814d90e4a7
Sagar Pant <sagar_007p@yahoo.com>
2012-04-22 00:12:46 -0500

    modified:   app/controllers/graphs_controller.rb

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -206,7 +206,7 @@ class GraphsController < ApplicationController
 	
 	@dates.each do |d|
 	
-		@categories = Category.where("categorytype= ? and date_spent = ?",c.categorytype,d.date_spent).select(" categorytype, sum(expense)").Group("categorytype")
+		@categories = Category.where("date_spent = ?" , d.date_spent).select(" categorytype, sum(expense) as expn ").Group("categorytype")
 		@categories.each do |category|
 				cat_id = category[0].to_i
 				if cat_id == 0 
